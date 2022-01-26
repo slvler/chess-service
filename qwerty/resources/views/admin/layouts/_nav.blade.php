31a23c11c77c9a658c09eea6d036fa46eb2dec3d
hs-qwerty <hselviler1@gmail.com>
2022-01-26 23:42:55 +0300

    build

---
+++ qwerty/resources/views/admin/layouts/_nav.blade.php
@@ -0,0 +1,101 @@
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+             with font-awesome or any other icon font library -->
+
+        <li class="nav-header">Genel</li>
+        <li class="nav-item">
+            <a href="pages/calendar.html" class="nav-link">
+                <i class="nav-icon fa fa-list"></i>
+                <p>
+                    Ayarlar
+                    <span class="badge badge-info right">2</span>
+                </p>
+            </a>
+        </li>
+
+
+        <li class="nav-header">İçerikler</li>
+        <li class="nav-item menu-open">
+            <a href="#" class="nav-link active">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>
+                    İçerikler
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="./index.html" class="nav-link active">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Dashboard v1</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="./index2.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Dashboard v2</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="./index3.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Dashboard v3</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+
+        <li class="nav-item menu-open">
+            <a href="#" class="nav-link active">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>
+                    Sliderlar
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="./index.html" class="nav-link active">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Dashboard v1</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="./index2.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Dashboard v2</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="./index3.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Dashboard v3</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+
+        <li class="nav-header">Listeler</li>
+        <li class="nav-item">
+            <a href="{{ route('menuAdmin') }}" class="nav-link">
+                <i class="nav-icon fa fa-list"></i>
+                <p>
+                    Menüler
+                    <span class="badge badge-info right">2</span>
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pages/calendar.html" class="nav-link">
+                <i class="nav-icon fa fa-list"></i>
+                <p>
+                    Kategoriler
+                    <span class="badge badge-info right">2</span>
+                </p>
+            </a>
+        </li>
+
+
+    </ul>
+</nav>
---
+++ qwerty/resources/views/admin/menu/index.blade.php
@@ -0,0 +1,113 @@
+@extends('admin.main')
+
+@section('title')
+    Menu Listesi
+@stop
+
+@section('css')
+
+@stop
+
+@section('content')
+
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Dashboard</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard v1</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Small boxes (Stat box) -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title"></h3>
+                                <a href="{{ route('menuAdminCreate') }}"><button class="btn btn-primary">Menü Ekle</button></a>
+
+                                <div class="card-tools">
+                                    <div class="input-group input-group-sm" style="width: 150px;">
+                                        <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+
+                                        <div class="input-group-append">
+                                            <button type="submit" class="btn btn-default">
+                                                <i class="fas fa-search"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover text-nowrap">
+                                    <thead>
+                                    <tr>
+                                        <th>Sıra</th>
+                                        <th>Başlık</th>
+                                        <th>Açıklama</th>
+                                        <th>Detay</th>
+                                        <th>Durum</th>
+                                        <th>İşlemler</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+
+
+
+                                    @foreach($menu as $m)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $m->title }}</td>
+                                        <td>{{ $m->body }}</td>
+                                        <td>{{ $m->detail }}</td>
+                                        <td>{{ $m->status }}</td>
+                                        <td>
+                                            <a href="{{ route('menuAdminEdit', $m->id) }}"><button class="btn btn-success btn-xs">Güncelleme</button></a>
+                                            <a href=""><button class="btn btn-dark btn-xs">Alt Menü</button></a>
+                                            <a href=""><button class="btn btn-danger btn-xs">Sil</button></a>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+
+
+                    <!-- ./col -->
+                </div>
+                <!-- /.row -->
+                <!-- Main row -->
+                <div class="row">
+
+                </div>
+                <!-- /.row (main row) -->
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+
+@stop
+
+@section('js')
+
+@stop
